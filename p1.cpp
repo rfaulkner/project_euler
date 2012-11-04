bce05ee78df596c23fc9a92b2a3e925ff8b9688d
rfaulk <rfaulkner@wikimedia.org>
2012-11-03 19:15:21 -0700

    modified "<=" to "<".  `1000` was being added to the final sum.

--- p1.cpp
+++ p1.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int main() {
   int sum = 0;  
-  for (int i=0; i <= 1000; i++)
+  for (int i=0; i < 1000; i++)
     if (i % 3 == 0 || i % 5 == 0)
       sum += i;
   cout << sum << endl;
