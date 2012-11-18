d99fd079ba01783fae914e8c93bd1f74e1ea12e5
rfaulk <rfaulkner@wikimedia.org>
2012-11-17 17:32:06 -0800

    Fixed implementation to ignore non-edges and to properly build adjacency list.  Problem solved.

--- p107.java
+++ p107.java
@@ -50,6 +50,22 @@ public class p107 {
 		    }
 	}
 	
+	public static void print() {
+		for (int i = 0; i < p107.num_nodes; i++) {
+			for (int j = 0; j < p107.num_nodes; j++)
+				System.out.print(p107.adj_list[i][j] + " ");
+			System.out.print("\n");
+		}
+		System.out.println();
+	}
+	public static int total_cost() {
+		int cost = 0;
+		for (int i = 0; i < p107.num_nodes; i++) 
+			for (int j = 0; j < p107.num_nodes; j++)
+				if (adj_list[i][j] != -1)
+					cost += adj_list[i][j];
+		return cost / 2;
+	}
 	/*
 	 * Create a minimum spanning tree of graph from network.txt. To do this a greedy approach is used where least 
 	 * weight edge is added at each step to expand the network.
@@ -72,9 +88,10 @@ public class p107 {
 		p107.visited[node_index] = true;
 		
 		// Add the edges of node 0 to the priority qu 
-		for (int i = 0; i < p107.num_nodes; i++)
-			p.add(new Edge(0,i,p107.adj_list[0][i]));
-		
+		for (int i = 0; i < p107.num_nodes; i++) 
+			if (p107.adj_list[0][i] != -1)		// only add edges that exist
+				p.add(new Edge(0,i,p107.adj_list[0][i]));		
+	
 		while(p.size() > 0) {			
 			new_node = -1;
 			e = p.peek();
@@ -83,15 +100,19 @@ public class p107 {
 				new_node = e.get_node1();
 			else if (!p107.visited[e.get_node2()]) 
 				new_node = e.get_node2();
-			
-			if (new_node != -1) {
-				edgelist.add(e);
-				p107.visited[new_node] = true;
-				for (int i = 0; i < p107.num_nodes; i++)
-					p.add(new Edge(new_node,i,p107.adj_list[0][i]));
+			else {
+				p.remove(e);	// the edge leads to visited node.  remove it from the Queue and continue
+				continue;
 			}
 			
+			edgelist.add(e);
+			p107.visited[new_node] = true;
+			for (int i = 0; i < p107.num_nodes; i++)	// add the new node edges to the list
+				if (p107.adj_list[new_node][i] != -1)		// only add edges that exist
+					p.add(new Edge(new_node,i,p107.adj_list[new_node][i]));
+			
 			p.remove(e);
+			
 		}
 		
 		// Finally create the new graph
@@ -110,18 +131,26 @@ public class p107 {
 				
 		while(it.hasNext()) {
 			e = it.next();
-			p107.adj_list[e.get_node1()][e.get_node2()] = e.get_weight(); }
+			p107.adj_list[e.get_node1()][e.get_node2()] = e.get_weight(); 
+			p107.adj_list[e.get_node2()][e.get_node1()] = e.get_weight();
+		}
 	}
 	
 	public static void main(String[] args) {
-		try {
+		int cost_before, cost_after;
+		try {			
 			p107.readfile("network.txt");
+			cost_before = p107.total_cost();
+			p107.print();
+			
 			p107.mst();
-			for (int i = 0; i < p107.num_nodes; i++) {
-				for (int j = 0; j < p107.num_nodes; j++)
-					System.out.print(p107.adj_list[i][j] + " ");
-				System.out.print("\n");
-			}
+			cost_after = p107.total_cost();
+			p107.print();
+			
+			System.out.println("Cost before = " + cost_before);
+			System.out.println("Cost after = " + cost_after);
+			System.out.println("Savings = " + (cost_before - cost_after));
+			
 		} catch (Exception e) { return; }
 		
 	}
