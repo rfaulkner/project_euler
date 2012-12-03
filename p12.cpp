71a7c0db3cefe0fade18e06983f14975ba42d003
rfaulk <rfaulkner@wikimedia.org>
2012-12-03 15:36:05 -0800

    MOD: Taking a new approach, storing factors in vector then counting all unique divisors arising from those factors.

--- p12.cpp
+++ p12.cpp
@@ -9,7 +9,8 @@
 //
 
 #include <iostream>
-#include <string>
+#include <vector>
+#include <iterator>
 
 #if defined __GNUC__ || defined __APPLE__
 #include <ext/hash_map>
@@ -17,62 +18,58 @@
 #include <hash_map>
 #endif
 
-
 using namespace std;
 
 // Hash that stores number of divisors
-typedef __gnu_cxx::hash_map <long, int> map_t;
+typedef std::vector <long> my_vec;
+typedef __gnu_cxx::hash_map <long, bool> map_t;
 
-map_t map;
-map_t::iterator iter;
-
-int count_divisors(long n) {
+my_vec divisors_set(long n) {
     
-    int count = 2; // 1 & n are divisors
-    long i = n/2;
+    my_vec factors;
+    long i = 1;
     
-    // Check whether this value has a already a stored number of divisors
-    iter = map.find(n); 
-    if (iter != map.end())
-        return iter->second;
-    
-    while (i > 1)
-        if (n % i-- == 0)
-            count++;
-
-    map[n] = count; // store the divisors in the hash_map
-    return count;
+    while (i <= n/2) if (n % i++ == 0) factors.push_back(i);
+    return factors;
 }
 
 int main() {
     
     long index = 2;
-    int n, n_plus_1;    
-    int num_divisors;
+    map_t all_divisors;
     long tri_num;
     
-    // note that for triangle number k, T(k) = n(n+1) / 2
-    n = count_divisors(index);
-    while (true) {
-        n_plus_1 = (count_divisors(++index)); // count the divisors for the factor n+1
-        tri_num = (index - 1) * (index) / 2;
-        
-        num_divisors = n + n_plus_1;
-        num_divisors -= (tri_num % 2 == 0) ? 1 : 2;
-        map[tri_num] = num_divisors;
-        
-        // map[(index - 1) * (index) / 2] = num_divisors;
-        if (num_divisors >= 500) 
-        n = n_plus_1;        
-        
-        cout << "The " << (index - 1) << "th triangle number is: " << tri_num << endl;
-        cout << "Number of divisors: " << num_divisors << endl;
-        for (map_t::iterator i(map.begin()), j(map.end()); i!=j; ++i) cout << i->first << " : " << i->second << ", ";
-        cout << endl << endl;
-        
-        if (index == 10) break;
-    };
+    my_vec n_factors, n_plus_1_factors;
     
+    // note that for triangle number k, T(k) = n(n+1) / 2
+    n_factors = divisors_set(index);
+    while (true) {
+
+        n_plus_1_factors = divisors_set(++index);
+        tri_num = (index - 1) * (index) / 2;
+
+        for (my_vec::iterator s1(n_factors.begin()), e1(n_factors.end()); s1!=e1; s1++)
+            for (my_vec::iterator s2(n_plus_1_factors.begin()), e2(n_plus_1_factors.end()); s2!=e2; s2++) {
+                cout << (long)*s1 << " , " << (long)*s2 << " : ";
+                all_divisors[(long)*s1 * (long)*s2] = true;
+            }
+        cout << endl;
+        // for (map_t::iterator i(all_divisors.begin()), j(all_divisors.end()); i!=j; ++i) cout << i->first << " , ";
+        // cout << endl;
+        
+        cout << tri_num << ", the " << index << "th triangle number has " << all_divisors.size() << " divisors." << endl; // logging
+        if (all_divisors.size() >= 500) break; // break condition 
+        
+        n_factors = n_plus_1_factors;
+        
+        if (index == 20) break;
+    };
+
+//    cout << "The " << index << "th triangle number is: " << tri_num << endl;
+ //   cout << "Number of divisors: " << num_divisors << endl;
+  //  for (map_t::iterator i(map.begin()), j(map.end()); i!=j; ++i) cout << i->first << " : " << i->second << ", ";
+   // cout << endl << endl;
+
     return 0;
 }
 
