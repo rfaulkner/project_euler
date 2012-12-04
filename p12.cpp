f25cccf6508eada9a09e56b189ac8508d1d729cb
rfaulk <rfaulkner@wikimedia.org>
2012-12-03 17:03:47 -0800

    MOD: Complete.  Solution = 76576500.

--- p12.cpp
+++ p12.cpp
@@ -26,49 +26,66 @@ typedef __gnu_cxx::hash_map <long, bool> map_t;
 
 my_vec divisors_set(long n) {
     
-    my_vec factors;
-    long i = 1;
+    my_vec divisors;
+    long i = 2;
     
-    while (i <= n/2) if (n % i++ == 0) factors.push_back(i);
-    return factors;
+    divisors.push_back(1);
+    divisors.push_back(n);
+    
+    while (i <= n/2) 
+        if (n % i == 0) {
+            divisors.push_back(i);
+            i++;
+        } else
+            i++;
+    return divisors;
 }
 
 int main() {
     
-    long index = 2;
+    long index = 3, prod, compound_divisor;
     map_t all_divisors;
     long tri_num;
     
     my_vec n_factors, n_plus_1_factors;
     
     // note that for triangle number k, T(k) = n(n+1) / 2
-    n_factors = divisors_set(index);
-    while (true) {
 
-        n_plus_1_factors = divisors_set(++index);
+    n_factors = divisors_set(index);
+    
+    // for (my_vec::iterator it = n_factors.begin(); it != n_factors.end(); it++) cout << (long)*it << " ";
+    // cout << endl;
+    
+    while (true) {
+        
+        all_divisors.clear();
+
+        prod = (++index % 2 == 0) ? index / 2 : index; 
+        n_plus_1_factors = divisors_set(prod);
         tri_num = (index - 1) * (index) / 2;
 
+        for (my_vec::iterator s(n_factors.begin()), e(n_factors.end()); s!=e; s++) all_divisors[(long)*s] = true;
+        for (my_vec::iterator s(n_plus_1_factors.begin()), e(n_plus_1_factors.end()); s!=e; s++) all_divisors[(long)*s] = true;             
+            
         for (my_vec::iterator s1(n_factors.begin()), e1(n_factors.end()); s1!=e1; s1++)
             for (my_vec::iterator s2(n_plus_1_factors.begin()), e2(n_plus_1_factors.end()); s2!=e2; s2++) {
-                cout << (long)*s1 << " , " << (long)*s2 << " : ";
-                all_divisors[(long)*s1 * (long)*s2] = true;
+                // cout << (long)*s1 << " , " << (long)*s2 << " : ";
+                compound_divisor = (long)*s1 * (long)*s2;
+                if (tri_num % compound_divisor == 0)
+                    all_divisors[compound_divisor] = true;
             }
-        cout << endl;
-        // for (map_t::iterator i(all_divisors.begin()), j(all_divisors.end()); i!=j; ++i) cout << i->first << " , ";
-        // cout << endl;
-        
-        cout << tri_num << ", the " << index << "th triangle number has " << all_divisors.size() << " divisors." << endl; // logging
-        if (all_divisors.size() >= 500) break; // break condition 
-        
+                        
+        if (all_divisors.size() >= 500) break; // break condition         
         n_factors = n_plus_1_factors;
         
-        if (index == 20) break;
+        if (index % 1000 == 0) cout << tri_num << ", the " << index-1 << "th triangle number has " << all_divisors.size() << " divisors." << endl; // logging
+        // for (map_t::iterator i(all_divisors.begin()), j(all_divisors.end()); i!=j; ++i) cout << i->first << " , "; cout << endl; // testing
     };
 
-//    cout << "The " << index << "th triangle number is: " << tri_num << endl;
- //   cout << "Number of divisors: " << num_divisors << endl;
-  //  for (map_t::iterator i(map.begin()), j(map.end()); i!=j; ++i) cout << i->first << " : " << i->second << ", ";
-   // cout << endl << endl;
+    cout << endl << "The " << index-1 << "th triangle number is: " << tri_num << endl;
+    cout << "Number of divisors: " << all_divisors.size() << endl;
+    //  for (map_t::iterator i(map.begin()), j(map.end()); i!=j; ++i) cout << i->first << " : " << i->second << ", ";
+    // cout << endl << endl;
 
     return 0;
 }
