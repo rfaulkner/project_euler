500f546e0aad69d5255969345e21e92d0096a2f7
rfaulk <rfaulkner@wikimedia.org>
2013-04-21 22:31:00 -0700

    fix. namespace ref. indentation.

--- helper/big_number.cpp
+++ helper/big_number.cpp
@@ -11,99 +11,102 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+
 class BigNumber {
 
-public:
+    public:
 
-    BigNumber(int dim) {
-        this->digits = new int[dim];
-        this->size = dim;
-        for (int i = 0; i < dim; i++) this->digits[i] = 0;
-    }
-
-    BigNumber(int dim, long val) {
-        int i;
-
-        this->digits = new int[dim];
-        this->size = dim;
-        for (i = 0; i < dim; i++) {
-            this->digits[i] = val % 10;
-            val /= 10;
+        BigNumber(int dim) {
+            this->digits = new int[dim];
+            this->size = dim;
+            for (int i = 0; i < dim; i++) this->digits[i] = 0;
         }
-    }
 
-    /*
-     * Copy the argument
-     */
-    BigNumber(BigNumber& other) {
+        BigNumber(int dim, long val) {
+            int i;
 
-        this->size = other.get_size();
-        this->digits = new int[this->size];
+            this->digits = new int[dim];
+            this->size = dim;
+            for (i = 0; i < dim; i++) {
+                this->digits[i] = val % 10;
+                val /= 10;
+            }
+        }
 
-        for (int i = 0; i < this->size; i++)
-            digits[i] = other.get_digit(i);
-    }
+        /*
+         * Copy the argument
+         */
+        BigNumber(BigNumber& other) {
 
-    // Prints the number
-    void print() {
-        for (int i = this->size; i >= 0; i--) cout << this->digits[i];
-        cout << endl;
-    }
+            this->size = other.get_size();
+            this->digits = new int[this->size];
 
-    int get_size() { return this->size; }
-    int get_digit(int index) { return this->digits[index]; }
+            for (int i = 0; i < this->size; i++)
+                digits[i] = other.get_digit(i);
+        }
 
-    /*
-     * Adds this big number by the arg - size of operands
-     */
-    BigNumber* operator+(BigNumber& other)
-    {
-        int i, new_val, carry = 0;
+        // Prints the number
+        void print() {
+            for (int i = this->size; i >= 0; i--) cout << this->digits[i];
+            cout << endl;
+        }
 
-        // TODO - throw exception instead
-        if (this->size != other.get_size())
+        int get_size() { return this->size; }
+        int get_digit(int index) { return this->digits[index]; }
+
+        /*
+         * Adds this big number by the arg - size of operands
+         */
+        BigNumber* operator+(BigNumber& other)
         {
-            cout << "BigNumber::operator+ - Mismatched sizes." << endl;
-            return new BigNumber(1,0);
+            int i, new_val, carry = 0;
+
+            // TODO - throw exception instead
+            if (this->size != other.get_size())
+            {
+                cout << "BigNumber::operator+ - Mismatched sizes." << endl;
+                return new BigNumber(1,0);
+            }
+
+            for (i=0; i < this->size; i++)
+            {
+                new_val = digits[i] + other.get_digit(i) + carry;
+
+                this->digits[i] = new_val % 10;
+                if (new_val >= 10)
+                    carry = new_val / 10;
+                else
+                    carry = 0;
+            }
+
+            // TODO - throw exception instead
+            if (carry > 0)
+                cout << "BigNumber::operator+ - Overflow." << endl;
+
+            return this;
         }
 
-        for (i=0; i < this->size; i++)
-        {
-            new_val = digits[i] + other.get_digit(i) + carry;
+        /*
+         * Multiplies this big number by the arg
+         */
+        BigNumber* multiply(int other) {
 
-            this->digits[i] = new_val % 10;
-            if (new_val >= 10)
-                carry = new_val / 10;
-            else
-                carry = 0;
+            int i, new_val, carry = 0;
+
+            for (i=0; i < this->size; i++) {
+                new_val = digits[i] * other + carry;
+
+                this->digits[i] = new_val % 10;
+                if (new_val >= 10)
+                    carry = new_val / 10;
+                else
+                    carry = 0;
+            }
+
+            return this;
         }
-
-        // TODO - throw exception instead
-        if (carry > 0)
-            cout << "BigNumber::operator+ - Overflow." << endl;
-
-        return this;
-    }
-
-    /*
-     * Multiplies this big number by the arg
-     */
-    BigNumber* multiply(int other) {
-
-        int i, new_val, carry = 0;
-
-        for (i=0; i < this->size; i++) {
-            new_val = digits[i] * other + carry;
-
-            this->digits[i] = new_val % 10;
-            if (new_val >= 10)
-                carry = new_val / 10;
-            else
-                carry = 0;
-        }
-
-        return this;
-    }
 
     private:
         int* digits;
